e838a611bb078a8392745df9076fb672e0a2686c
OnurK.Dev <onurk.dev@outlook.com>
2021-10-24 09:57:49 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿
+namespace BA_Boost_Toplayici
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sumbutton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.clearbutton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // sumbutton
+            // 
+            this.sumbutton.Location = new System.Drawing.Point(343, 166);
+            this.sumbutton.Name = "sumbutton";
+            this.sumbutton.Size = new System.Drawing.Size(100, 40);
+            this.sumbutton.TabIndex = 0;
+            this.sumbutton.Text = "Topla";
+            this.sumbutton.UseVisualStyleBackColor = true;
+            this.sumbutton.Click += new System.EventHandler(this.sumbutton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(246, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(305, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Lütfen toplanack sayıları tek satırda giriniz.";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(518, 95);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(270, 144);
+            this.listBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Candara", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(337, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(121, 35);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Toplayıcı";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(272, 122);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(240, 26);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // clearbutton
+            // 
+            this.clearbutton.Location = new System.Drawing.Point(343, 212);
+            this.clearbutton.Name = "clearbutton";
+            this.clearbutton.Size = new System.Drawing.Size(100, 40);
+            this.clearbutton.TabIndex = 5;
+            this.clearbutton.Text = "Temizle";
+            this.clearbutton.UseVisualStyleBackColor = true;
+            this.clearbutton.Click += new System.EventHandler(this.clearbutton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 286);
+            this.Controls.Add(this.clearbutton);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.sumbutton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button sumbutton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button clearbutton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BA_Boost_Toplayici
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string input;
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            input = textBox1.Text;
+        }
+
+        private void sumbutton_Click(object sender, EventArgs e)
+        {
+            string message="";
+            int sumnumbers=0;
+            char[] chars = input.Trim().ToCharArray();
+            foreach (char c in chars) {
+                int num;
+                try
+                {
+                    num = Convert.ToInt32(Char.GetNumericValue(c));
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Lütfen sadece sayı giriniz");
+                    return;
+                }
+                sumnumbers += num;
+
+                
+
+            }
+            foreach (char c in chars) {
+
+                message = message + " + " + c;
+
+            }
+            string messagenew = message.Remove(1,1) + " => "+sumnumbers;
+            listBox1.Items.Add(messagenew);
+
+        }
+
+        private void clearbutton_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+
+        }
+    }
+}
